1dfb330d53c07d08cdb62dde2dd420681d826878
imsrkno1 <68878820+imsrkno1@users.noreply.github.com>
2025-03-24 04:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,7 @@
-from flask import Flask, jsonify
-import fitz  # PyMuPDF
-
-app = Flask(__name__)
-
-PDF_PATH = "Updated_Template.pdf"
-
-@app.route("/debug_annots", methods=["GET"])
-def debug_annots():
+@app.route("/check_pymupdf", methods=["GET"])
+def check_pymupdf():
     try:
-        doc = fitz.open(PDF_PATH)
-        fields_annots = []
-
-        for page in doc:
-            for field in page.annots():
-                fields_annots.append(field.info.get("title", "Unknown Field"))
-
-        doc.close()
-
-        return jsonify({"annotations": fields_annots})
-
+        import pymupdf
+        return jsonify({"PyMuPDF Version": pymupdf.__version__})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
